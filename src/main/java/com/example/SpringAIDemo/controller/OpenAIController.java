d7fb8a27e00e2d32d772a86eb7418cde68f03f93
manirathnam5 <manirathnam501@gmail.com>
2026-07-03 16:33:50 +0530

    open ai controller

--- src/main/java/com/example/SpringAIDemo/controller/OpenAIController.java
+++ src/main/java/com/example/SpringAIDemo/controller/OpenAIController.java
@@ -1,4 +1,30 @@
+/*
 package com.example.SpringAIDemo.controller;
 
-public class OpenaiController {
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/openai")
+@CrossOrigin("*")
+public class OpenAIController {
+
+
+
+    private OpenAiChatModel chatModel;
+
+
+    private OpenAIController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+        String response = chatModel.call(message);
+        return ResponseEntity.ok(response);
+    }
+
 }
+*/

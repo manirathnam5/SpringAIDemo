dc30dcc15ae0d0b31bd1c1f7f74de2ad4ba782b5
manirathnam5 <manirathnam501@gmail.com>
2026-07-03 16:33:24 +0530

    ollama controller

--- src/main/java/com/example/SpringAIDemo/controller/OllamaController.java
+++ src/main/java/com/example/SpringAIDemo/controller/OllamaController.java
@@ -1,4 +1,31 @@
 package com.example.SpringAIDemo.controller;
 
+
+import org.springframework.ai.anthropic.AnthropicChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/ollama")
+@CrossOrigin("*")
 public class OllamaController {
+
+
+
+    private OllamaChatModel chatModel;
+
+
+    private OllamaController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+        String response = chatModel.call(message);
+        return ResponseEntity.ok(response);
+    }
+
+
+
 }

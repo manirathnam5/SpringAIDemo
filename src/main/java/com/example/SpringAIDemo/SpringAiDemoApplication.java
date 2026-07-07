e68d39acd0794320ab4ceadb23c08d862f29575e
manirathnam5 <manirathnam501@gmail.com>
2026-07-07 23:13:46 +0530

    spring ai

---
+++ src/main/java/com/example/SpringAIDemo/SpringAiDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.SpringAIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiDemoApplication.class, args);
+
+        System.out.println("Spring AI application demo  ");
+	}
+
+
+}
